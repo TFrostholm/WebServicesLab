dcef8acb398b8b1f934059d41c6caf82c06280c3
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-04-04 10:27:51 +0200

    Added HotelViewModel with reference to HotelCatalogSingleton

---
+++ HotelMVVM/ViewModel/HotelViewModel.cs
@@ -0,0 +1,14 @@
+﻿using HotelMVVM.Model;
+
+namespace HotelMVVM.ViewModel
+{
+    public class HotelViewModel
+    {
+        public HotelCatalogSingleton HotelCatalogSingleton { get; set; }
+        public HotelViewModel()
+        {
+            HotelCatalogSingleton = HotelCatalogSingleton.Instance;
+
+        }
+    }
+}
