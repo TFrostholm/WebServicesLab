538a3622cbfd4730544f7b4d49ffaaa637b70bf3
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-04-04 10:26:41 +0200

    Added a singleton with an ObservableCollection Hotels of hotel object. Initialized a few objects of type hotel in constructor

---
+++ HotelMVVM/Model/HotelCatalogSingleton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+
+namespace HotelMVVM.Model
+{
+    public class HotelCatalogSingleton
+    {
+        private static HotelCatalogSingleton instance = new HotelCatalogSingleton();
+
+        public static HotelCatalogSingleton Instance
+        {
+            get { return instance; }
+        }
+
+        public ObservableCollection<Hotel> Hotels { get; set; }
+
+        private HotelCatalogSingleton()
+        {
+            Hotels = new ObservableCollection<Hotel>();
+            Hotel h1 = new Hotel(101, "London Hotel", "London Boulevard");
+            Hotel h2 = new Hotel(102, "Berlin Hotel", "Berliner garten");
+            Hotels.Add(h1);
+            Hotels.Add(h2);
+        }
+
+        public void Add(int Hotel_No, string Name, string Address)
+        {
+            Hotels.Add(new Hotel(Hotel_No, Name, Address));
+        }
+    }
+}
