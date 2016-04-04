52311ebf4341f469958cb03f8786f2976a356cc9
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-04-04 10:24:19 +0200

    Added Hotel class with properties, constructor and ToString method

---
+++ HotelMVVM/Model/Hotel.cs
@@ -0,0 +1,23 @@
+﻿namespace HotelMVVM.Model
+{
+    public class Hotel
+    {
+        public int Hotel_No { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public Hotel(int hotelNo, string name, string address)
+        {
+            Hotel_No = hotelNo;
+            Name = name;
+            Address = address;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Hotelno {0} Name {1} Address {2}", Hotel_No, Name, Address);
+        }
+    }
+}
