a80509b57f6aa8b6faf4956f351e7ce4a2fd4af5
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-04-18 11:51:15 +0200

    Bound controls in RoomPage to properties of newly created property NewRoom

--- HotelMVVM/ViewModel/RoomViewModel.cs
+++ HotelMVVM/ViewModel/RoomViewModel.cs
@@ -7,12 +7,22 @@ namespace HotelMVVM.ViewModel
     public class RoomViewModel : INotifyPropertyChanged
     {
         public HotelCatalogSingleton HotelCatalogSingleton { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public RoomViewModel()
         {
             HotelCatalogSingleton = HotelCatalogSingleton.Instance;
-
         }
 
+        private Room _newRoom;
+
+        public Room NewRoom
+        {
+            get { return _newRoom; }
+            set { _newRoom = value; OnPropertyChanged(); }
+        }
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged([CallerMemberName]     string propertyName = null)
