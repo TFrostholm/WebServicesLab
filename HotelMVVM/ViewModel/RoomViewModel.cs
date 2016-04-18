24ba367f0c81267756f9b96f27b487a0e4a97803
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-04-18 12:14:34 +0200

    Added CreateRoomCommand

--- HotelMVVM/ViewModel/RoomViewModel.cs
+++ HotelMVVM/ViewModel/RoomViewModel.cs
@@ -1,28 +1,45 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using HotelMVVM.Handler;
 using HotelMVVM.Model;
 
 namespace HotelMVVM.ViewModel
 {
     public class RoomViewModel : INotifyPropertyChanged
     {
+        // reference to singleton
         public HotelCatalogSingleton HotelCatalogSingleton { get; set; }
 
+        // Refence to RoomHandler
+        public Handler.RoomHandler RoomHandler { get; set; }
+            
         public RoomViewModel()
         {
             HotelCatalogSingleton = HotelCatalogSingleton.Instance;           
 
             //Creates an instance of the newHotel
             _newRoom = new Room();
+
+            // New instance of RoomHandler
+            RoomHandler = new Handler.RoomHandler(this);
         }
 
         private Room _newRoom;
+        private ICommand _createRoomCommand;
 
         public Room NewRoom
         {
             get { return _newRoom; }
             set { _newRoom = value; OnPropertyChanged(); }
         }
+
+        public ICommand CreateRoomCommand
+        {
+            get { return _createRoomCommand; }
+            set { _createRoomCommand = value; }
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged([CallerMemberName]     string propertyName = null)
