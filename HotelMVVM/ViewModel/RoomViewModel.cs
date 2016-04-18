2a18eec5ac1575cb3e26342256e5688630ebe2b7
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-04-18 11:40:41 +0200

    Implemented INotifyPropertyChanged in RoomViewModel

--- HotelMVVM/ViewModel/RoomViewModel.cs
+++ HotelMVVM/ViewModel/RoomViewModel.cs
@@ -1,14 +1,24 @@
-﻿using HotelMVVM.Model;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using HotelMVVM.Model;
 
 namespace HotelMVVM.ViewModel
 {
-    public class RoomViewModel
+    public class RoomViewModel : INotifyPropertyChanged
     {
         public HotelCatalogSingleton HotelCatalogSingleton { get; set; }
         public RoomViewModel()
         {
             HotelCatalogSingleton = HotelCatalogSingleton.Instance;
 
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]     string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
